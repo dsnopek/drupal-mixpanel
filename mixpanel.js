274d9a6b4adaaac758d964c497ba6080b3f6664f
David Snopek <dsnopek@gmail.com>
2012-11-24 21:46:45 +0000

    Added support for the 2.0 version of the Javascript library and 'People' profiles.

--- mixpanel.js
+++ mixpanel.js
@@ -1,10 +1,21 @@
 // Do some initial setup.
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-    // Uniquely identify the user;
-    mpq.push(["identify", Drupal.settings.mixpanel.defaults.uid]);
+    if (typeof mpq !== 'undefined') {
+      // Mixpanel library version 1.0
+      mpq.push(["identify", Drupal.settings.mixpanel.defaults.uid]);
+      mpq.push(["register", Drupal.settings.mixpanel.defaults]);
+    }
+    else {
+      // Uniquely identify the user.
+      mixpanel.identify(Drupal.settings.mixpanel.defaults.uid);
 
-    // Register properties about the user.
-    mpq.push(["register", Drupal.settings.mixpanel.defaults]);
+      // Register properties about the user.
+      mixpanel.register(Drupal.settings.mixpanel.defaults);
+
+      // Super basic support for 'People'
+      mixpanel.people.identify(Drupal.settings.mixpanel.defaults.uid);
+      mixpanel.people.set(Drupal.settings.mixpanel.people);
+    }
   });
 }
